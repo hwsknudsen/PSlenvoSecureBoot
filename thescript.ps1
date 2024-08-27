d7649811ccdf97bc9cde48f9b9f4a280b5b6a47b
hwsknudsen <hwsknudsen@users.noreply.github.com>
2024-08-27 11:37:18 -0300

    Update and rename thescript to thescript.ps1

--- thescript.ps1
+++ thescript.ps1
@@ -2,3 +2,8 @@ $computername = hostname
 (Get-WmiObject -Class Lenovo_SetBiosSetting -Namespace root\wmi -ComputerName $Computername -ErrorAction Stop).SetBiosSetting("SecureBoot,Enable,"",ascii,us")
 (Get-WmiObject -Class Lenovo_SaveBiosSettings -Namespace root\wmi -ComputerName $Computername -ErrorAction Stop).SaveBiosSettings(""",ascii,us")
 restart-computer -computername $Computername -Force
+
+
+
+#view all settings
+#(Get-WmiObject -Class Lenovo_BiosSetting -Namespace root\wmi -ComputerName $Computername).CurrentSetting | Where-Object {$_ -ne ""} | Sort-Object | Out-GridView
